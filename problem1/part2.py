82cafa59fcab975cde33f4b4c8b08649c6b3150e
stephenvsp <stephenvsp@gmail.com>
2020-12-01 10:42:55 -0500

    refactor part 2

--- problem1/part2.py
+++ problem1/part2.py
@@ -5,11 +5,14 @@ inputs = [int(i.strip()) for i in inputs]
 
 target = 2020
 
-for i in range(0, len(inputs) - 2):
-    for j in range(1, len(inputs) - 1):
-        for k in range(2, len(inputs)):
-            num_one = inputs[i]
-            num_two = inputs[j]
-            num_three = inputs[k]
-            if num_one + num_two + num_three == target:
-                print(num_one * num_two * num_three)
+def loop(inputs):
+    for i in range(0, len(inputs) - 2):
+        for j in range(1, len(inputs) - 1):
+            for k in range(2, len(inputs)):
+                num_one = inputs[i]
+                num_two = inputs[j]
+                num_three = inputs[k]
+                if num_one + num_two + num_three == target:
+                    return num_one * num_two * num_three
+
+print(loop(inputs))
