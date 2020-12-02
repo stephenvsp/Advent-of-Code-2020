2f64d9fe8b258948a9efd495b7ba847aa8a97dcf
stephenvsp <stephenvsp@gmail.com>
2020-12-02 09:40:41 -0500

    finish day 2 part 2

---
+++ problem2/part2.py
@@ -0,0 +1,25 @@
+with open('problem2_input.txt') as file:
+    inputs = file.readlines()
+
+
+total_count = 0
+for i in inputs:
+    split = i.split(' ')
+
+    bounds = split[0].split('-')
+    lower_bound = int(bounds[0])
+    upper_bound = int(bounds[1])
+
+    character = split[1][0]
+
+    password = split[2]
+
+    count = 0
+    for c in password:
+        if c == character:
+            count += 1
+
+    if (password[lower_bound - 1] == character) ^ (password[upper_bound - 1] == character):
+        total_count += 1
+
+print(total_count)
