1409da7b566d58e3fafc77adfedb936cfe8c9e93
stephenvsp <stephenvsp@gmail.com>
2020-12-05 16:46:33 -0500

    refactor

--- problem05/part1.py
+++ problem05/part1.py
@@ -1,8 +1,5 @@
 with open('problem5_input.txt') as file:
     inputs = file.readlines()
 
-highest = max([int(i.strip().replace('B','1').replace('F','0').replace('L','0').replace('R','1'),2) for i in inputs])
-
-
-print(highest)
+print(max([int(i.strip().replace('B','1').replace('F','0').replace('L','0').replace('R','1'), 2) for i in inputs]))
     
