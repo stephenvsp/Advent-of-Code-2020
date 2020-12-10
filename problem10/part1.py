59608677eba9347d31ca97899f807d214b3fd7fd
stephenvsp <stephenvsp@gmail.com>
2020-12-10 16:37:21 -0500

    solve d10p1

---
+++ problem10/part1.py
@@ -0,0 +1,26 @@
+with open('problem10_input.txt') as file:
+    jolts = file.read().splitlines()
+
+jolts = [int(i) for i in jolts]
+
+jolts = sorted(jolts)
+
+jolts = [0] + jolts + [jolts[-1] + 3]
+
+difference_of_one = 0
+difference_of_three = 0
+last = 0
+
+print(jolts)
+
+for jolt in jolts:
+    if jolt - last  == 1:
+        difference_of_one += 1
+    elif jolt - last == 3:
+        difference_of_three += 1
+
+    last = jolt
+
+print(difference_of_one)
+print(difference_of_three)
+print(difference_of_one * difference_of_three)
