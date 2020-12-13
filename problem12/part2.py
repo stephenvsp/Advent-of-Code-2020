71dd2db26d755a4e8aafcb09b7bbdf07c3361159
stephenvsp <stephenvsp@gmail.com>
2020-12-12 23:48:35 -0500

    CLEANER

--- problem12/part2.py
+++ problem12/part2.py
@@ -1,29 +1,24 @@
-import cmath
-
 with open('problem12_input.txt') as file:
     instructions = file.read().splitlines()
 
 ship = complex(0, 0)
 waypoint = complex(10, 1)
 
+directions = {'N':(0, 1), 'S':(0, -1), 'E':(1,0), 'W':(-1, 0)}
+rotation = {'R':(0, -1), 'L':(0, 1)}
+
 for i in instructions:
 
     action, magnitude = i[:1], int(i[1:])
 
-    if action == 'N':
-        waypoint += complex(0, magnitude)
-    elif action == 'S':
-        waypoint += complex(0, -magnitude)
-    elif action == 'E':
-        waypoint += complex(magnitude, 0)
-    elif action == 'W':
-        waypoint += complex(-magnitude, 0)
-    elif action == 'F':
+    if action in 'NSEW':
+        vector = directions[action]
+        waypoint += complex(vector[0] * magnitude, vector[1] * magnitude)
+    elif action in 'RL':
+        vector = rotation[action]
+        waypoint *= complex(vector[0], vector[1])**(magnitude // 90)
+    else:
         ship += waypoint * magnitude
-    elif action == 'R':
-        waypoint *= complex(0, -1)**(magnitude // 90)
-    elif action == 'L':
-        waypoint *= complex(0, 1)**(magnitude // 90)
 
 print(ship.real + ship.imag)
 
