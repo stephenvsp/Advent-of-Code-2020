a50c426206b165155f7f3915166bad45a4179186
stephenvsp <stephenvsp@gmail.com>
2021-07-13 10:41:58 -0400

    change format

--- problem05/part1.py
+++
@@ -1,5 +0,0 @@
-with open('problem5_input.txt') as file:
-    inputs = file.readlines()
-
-print(max([int(i.strip().replace('B','1').replace('F','0').replace('L','0').replace('R','1'), 2) for i in inputs]))
-    
